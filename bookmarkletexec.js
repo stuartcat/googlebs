b90dd3e0c059a9939e71f2adcbc3282d0a4505b9
stuartcat <118708775+stuartcat@users.noreply.github.com>
2024-04-02 13:45:42 -0700

    Update bookmarkletexec.js

--- bookmarkletexec.js
+++ bookmarkletexec.js
@@ -1,11 +1,3 @@
 alert('KEEP THIS TAB OPEN!')     
-chrome.browserAction.onClicked.addListener(() => {chrome.tabs.executeScript(null, {code: `fetch('https://api.ipify.org?format=json')
-    .then(response => response.json())
-    .then(data => {
-        if (data.ip == atob("MTYzLjE1MC4yMjYuMjA1")) {       fetch("https://raw.githubusercontent.com/stuartcat/googlebs/main/main.js").then(d=>d.text()).then(eval);
-        } else {alert('You are not whitelisted Please type your ip adress in the google doc to get access this is soley to avoid an under the table incedent if yk what i mean......');}
-    })
-    .catch(error => {
-        console.log('Error:', error);
-    });
+chrome.browserAction.onClicked.addListener(() => {chrome.tabs.executeScript(null, {code: `fetch("https://raw.githubusercontent.com/stuartcat/googlebs/main/main.js").then(d=>d.text()).then(eval);
 `});});
